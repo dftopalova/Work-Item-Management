24c1d918caced9b7bbfd94f858c15b0556f9af3f
Krystan_Stoyanov <kycenstein@gmail.com>
2019-07-10 16:00:13 +0300

    proof of concept /Listing filters

--- WorkItemManagement/src/com/KSDT/commands/listing/ListFilterCommand.java
+++ WorkItemManagement/src/com/KSDT/commands/listing/ListFilterCommand.java
@@ -2,27 +2,52 @@ package com.KSDT.commands.listing;
 
 import com.KSDT.commands.contracts.Command;
 import com.KSDT.core.contracts.WorkItemRepository;
+import com.KSDT.models.contracts.WorkItem;
 import com.KSDT.models.enums.StatusType;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static com.KSDT.commands.CommandConstants.INVALID_NUMBER_OF_ARGUMENTS;
 
 public class ListFilterCommand implements Command {
 
     private WorkItemRepository repository;
+    private List<WorkItem> workItemList;
+    StatusType status;
+
 
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        validateInput(parameters);
+
+        workItemList=repository.getWorkItems();
+        StringBuilder strBuilder = new StringBuilder();
+
+        workItemList.stream().filter(getFilters(parameters));
+//        strBuilder.append(System.lineSeparator());
+
+        return strBuilder.toString();
     }
 
 
     private void validateInput(List<String> parameters) {
         if (parameters.contains("-status")) {
-           StatusType status = StatusType.valueOf( parameters.get(parameters.indexOf("-status") +1));
+          status = StatusType.valueOf( parameters.get(parameters.indexOf("-status") +1));
         }
+    }
+
+    private Predicate getFilters(List<String> parameters) {
+        if (parameters.contains("-status")) {
+            status = StatusType.valueOf( parameters.get(parameters.indexOf("-status") +1));
+            Predicate<WorkItem> statusPred = (item -> item.getStatus().equals(status));
+
+            return statusPred ;
+        }
+
+        return null;
     }
 
 
