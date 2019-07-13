09ff2b535f4c114f6f4936a22dabbeb6450ae6d3
Krystan_Stoyanov <kycenstein@gmail.com>
2019-07-13 19:29:07 +0300

    added create Board/Feedback command tests

---
+++ WorkItemManagement/WIMTests/src/com/KSDT/tests/commands/CreateBoardInTeam_Tests.java
@@ -0,0 +1,102 @@
+package com.KSDT.tests.commands;
+
+import com.KSDT.commands.contracts.Command;
+import com.KSDT.commands.creation.CreateBoardInTeamCommand;
+import com.KSDT.commands.creation.CreateFeedbackInBoard;
+import com.KSDT.core.WorkItemRepositoryImpl;
+import com.KSDT.core.contracts.WorkItemFactory;
+import com.KSDT.core.contracts.WorkItemRepository;
+import com.KSDT.core.factories.WorkItemFactoryImpl;
+import com.KSDT.models.BoardImpl;
+import com.KSDT.models.TeamImpl;
+import com.KSDT.models.contracts.Board;
+import com.KSDT.models.contracts.Team;
+import com.KSDT.models.contracts.WorkItem;
+import com.KSDT.models.enums.StatusType;
+import com.KSDT.models.items.FeedbackImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateBoardInTeam_Tests {
+    private Command testCommand;
+    private WorkItemRepository repository;
+    private WorkItemFactory factory;
+    private Team testTeam;
+    private Board testBoard;
+
+    @Before
+    public void before() {
+        repository = new WorkItemRepositoryImpl();
+        factory = new WorkItemFactoryImpl();
+        testCommand = new CreateBoardInTeamCommand(repository, factory);
+        testTeam = new TeamImpl("testTeam");
+        testBoard = new BoardImpl("testBoard", testTeam);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenPassedLessArguments() {
+        // Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("asdasd");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenPassedMoreArguments() {
+        // Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenTeamDoesntExist() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testBoard");
+        testList.add("testTeam");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenBoardAlreadyExist() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testBoard");
+        testList.add("testTeam");
+        repository.addTeam("testTeam", testTeam);
+        testTeam.addBoard("testBoard", testBoard);
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test
+    public void execute_Should_CreateBoardWhenInputIsValid() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testBoard");
+        testList.add("testTeam");
+        repository.addTeam("testTeam", testTeam);
+
+        //Act
+        testCommand.execute(testList);
+
+        //Assert
+        Assert.assertEquals(1, testTeam.getBoardsList().size());
+    }
+
+
+}
---
+++ WorkItemManagement/WIMTests/src/com/KSDT/tests/commands/CreateFeedbackInBoard_Tests.java
@@ -0,0 +1,122 @@
+package com.KSDT.tests.commands;
+
+import com.KSDT.commands.contracts.Command;
+import com.KSDT.commands.creation.CreateFeedbackInBoard;
+import com.KSDT.core.WorkItemRepositoryImpl;
+import com.KSDT.core.contracts.WorkItemFactory;
+import com.KSDT.core.contracts.WorkItemRepository;
+import com.KSDT.core.factories.WorkItemFactoryImpl;
+import com.KSDT.models.BoardImpl;
+import com.KSDT.models.TeamImpl;
+import com.KSDT.models.contracts.Board;
+import com.KSDT.models.contracts.Team;
+import com.KSDT.models.contracts.WorkItem;
+import com.KSDT.models.enums.StatusType;
+import com.KSDT.models.items.FeedbackImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateFeedbackInBoard_Tests {
+    private Command testCommand;
+    private WorkItemRepository repository;
+    private WorkItemFactory factory;
+    private Team testTeam;
+    private WorkItem testWorkItem;
+    private Board testBoard;
+
+    @Before
+    public void before() {
+        repository = new WorkItemRepositoryImpl();
+        factory = new WorkItemFactoryImpl();
+        testCommand = new CreateFeedbackInBoard(repository, factory);
+        testTeam = new TeamImpl("testTeam");
+        testWorkItem = new FeedbackImpl("testFeedback", StatusType.FEEDBACK_SCHEDULED, "asd asd asd", 5);
+        testBoard = new BoardImpl("testBoard", testTeam);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenPassedLessArguments() {
+        // Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("asdasd");
+        testList.add("asdasd");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenPassedMoreArguments() {
+        // Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+        testList.add("asdasd");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenTeamDoesntExist() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testTeam");
+        testList.add("testBoard");
+        testList.add("testFeedback");
+        testList.add("Scheduled");
+        testList.add("5");
+        testList.add("asd asd asd");
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void execute_Should_ThrowExceptionWhenBoardDoesntExist() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testTeam");
+        testList.add("testBoard");
+        testList.add("testFeedback");
+        testList.add("Scheduled");
+        testList.add("5");
+        testList.add("asd asd asd");
+        repository.addTeam("testTeam", testTeam);
+
+        //Act & Assert
+        testCommand.execute(testList);
+    }
+
+    @Test
+    public void execute_Should_CreateFeedbackWhenInputIsValid() {
+        //Arrange
+        List<String> testList = new ArrayList<>();
+        testList.add("testTeam");
+        testList.add("testBoard");
+        testList.add("testFeedback");
+        testList.add("Scheduled");
+        testList.add("5");
+        testList.add("asd asd asd");
+        repository.addTeam("testTeam", testTeam);
+        testTeam.addBoard("testBoard", testBoard);
+
+        //Act
+        testCommand.execute(testList);
+
+        //Assert
+        Assert.assertEquals(1, testBoard.getWorkItemsList().size());
+    }
+
+}
